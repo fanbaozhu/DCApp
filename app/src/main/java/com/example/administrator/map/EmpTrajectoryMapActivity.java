c48a0b5fc0570c6f77e82c3dd759a45033f3d7e7
zjw950323 <zuoyouzhilian@qq.com>
2018-05-02 17:11:05 +0800

    人员动态轨迹

--- app/src/main/java/com/example/administrator/map/EmpTrajectoryMapActivity.java
+++ app/src/main/java/com/example/administrator/map/EmpTrajectoryMapActivity.java
@@ -5,13 +5,14 @@ import android.app.Activity;
 
 import android.os.Bundle;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 import android.view.View;
 
 
 import android.widget.Button;
 import android.widget.LinearLayout;
-
 
 
 import android.widget.TextView;
@@ -35,6 +36,7 @@ import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 
+import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.map.Polyline;
@@ -74,6 +76,13 @@ public class EmpTrajectoryMapActivity extends Activity {
     private Button btnQuery;
     private TextView tvDept;
     private TextView tvEmp;
+    private Marker mMoveMarker;
+    private Handler mHandler;
+
+    // 通过设置间隔时间和距离可以控制速度和图标移动的距离
+    private static final int TIME_INTERVAL = 80;
+    private static final double DISTANCE = 0.0001;
+
     String starttime;
     String endtime;
     String dept;
@@ -95,7 +104,7 @@ public class EmpTrajectoryMapActivity extends Activity {
     private int checkedItem = 0; //默认选中的item
     private ArrayList<Deptidname> listdept = new ArrayList<Deptidname>();
     private ArrayList<Empidname> listemp = new ArrayList<Empidname>();
-    List<LatLng> options = new ArrayList<LatLng>();
+    List<LatLng> options = new ArrayList<>();
 
 
     @Override
@@ -114,6 +123,7 @@ public class EmpTrajectoryMapActivity extends Activity {
         btnQuery = (Button) findViewById(R.id.btnQuery);
         tvDept = (TextView) findViewById(R.id.tvDept);
         tvEmp = (TextView) findViewById(R.id.tvEmp);
+        mHandler = new Handler(Looper.getMainLooper());
         mQueue = Volley.newRequestQueue(getApplicationContext());
         mBaiduMap = mMapView.getMap();
         GetDept();
@@ -139,7 +149,7 @@ public class EmpTrajectoryMapActivity extends Activity {
             @Override
             public void onClick(View view) {
 
-                final EmpDialogFragment empDialogFragment = EmpDialogFragment.newInstance("请选择部门",listdept);
+                final EmpDialogFragment empDialogFragment = EmpDialogFragment.newInstance("请选择部门", listdept);
                 empDialogFragment.setOnItemClickListener(new DialogListViewAdapter.OnItemClickListener() {
                     @Override
                     public void onItemClick(Deptidname deptidname) {
@@ -149,18 +159,18 @@ public class EmpTrajectoryMapActivity extends Activity {
                         empDialogFragment.dismiss();
                     }
                 });
-                empDialogFragment.show(getFragmentManager(),"EmpDialog");
+                empDialogFragment.show(getFragmentManager(), "EmpDialog");
             }
         });
 
         tvEmp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if( tvDept.getText().toString()==null||tvDept.getText().toString().equals("")){
-                    Toast.makeText(EmpTrajectoryMapActivity.this,"请先选择部门",Toast.LENGTH_SHORT).show();
+                if (tvDept.getText().toString() == null || tvDept.getText().toString().equals("")) {
+                    Toast.makeText(EmpTrajectoryMapActivity.this, "请先选择部门", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                final EmpNameDialogFragment empDialogFragment = EmpNameDialogFragment.newInstance("请选择人员",listemp);
+                final EmpNameDialogFragment empDialogFragment = EmpNameDialogFragment.newInstance("请选择人员", listemp);
                 empDialogFragment.setOnItemClickListener(new EmpNameDialogListViewAdapter.OnItemClickListener() {
 
                     @Override
@@ -172,7 +182,7 @@ public class EmpTrajectoryMapActivity extends Activity {
 
                     }
                 });
-                empDialogFragment.show(getFragmentManager(),"EmpDialog");
+                empDialogFragment.show(getFragmentManager(), "EmpDialog");
             }
         });
         btnQuery.setOnClickListener(new View.OnClickListener() {
@@ -186,11 +196,11 @@ public class EmpTrajectoryMapActivity extends Activity {
                 emp = tvEmp.getText().toString();
                 startt = tvStartTime.getText().toString();
                 endt = tvEndTime.getText().toString();
-                if (TextUtils.isEmpty(empdept)||TextUtils.isEmpty(emp)) {
+                if (TextUtils.isEmpty(empdept) || TextUtils.isEmpty(emp)) {
                     Toast.makeText(EmpTrajectoryMapActivity.this, "请选择部门和人员", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if (TextUtils.isEmpty(startt)||TextUtils.isEmpty(endt)) {
+                if (TextUtils.isEmpty(startt) || TextUtils.isEmpty(endt)) {
                     Toast.makeText(EmpTrajectoryMapActivity.this, "请选择时间段", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -202,11 +212,11 @@ public class EmpTrajectoryMapActivity extends Activity {
                     et = sdf.parse(endtime);
                     difference = et.compareTo(st);
                     if (difference >= 0) {
-                        getStringToDate(starttime,"yyyy-MM-dd HH:mm");
-                        String stt = stamp+"";
-                        getStringToDate(endtime,"yyyy-MM-dd HH:mm");
-                        String edt = stamp+"";
-                        Gettrajectory(simid,stt,edt);
+                        getStringToDate(starttime, "yyyy-MM-dd HH:mm");
+                        String stt = stamp + "";
+                        getStringToDate(endtime, "yyyy-MM-dd HH:mm");
+                        String edt = stamp + "";
+                        Gettrajectory(simid, stt, edt);
                     } else {
                         Toast.makeText(EmpTrajectoryMapActivity.this, "请选择正确的结束时间", Toast.LENGTH_SHORT).show();
                         return;
@@ -221,13 +231,13 @@ public class EmpTrajectoryMapActivity extends Activity {
     public static long getStringToDate(String dateString, String pattern) {
         SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
         Date date = new Date();
-        try{
+        try {
             date = dateFormat.parse(dateString);
-        } catch(ParseException e) {
+        } catch (ParseException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        stamp = date.getTime()/1000;
+        stamp = date.getTime() / 1000;
         return stamp;
     }
 
@@ -311,7 +321,8 @@ public class EmpTrajectoryMapActivity extends Activity {
                 });
         mQueue.add(stringRequest);
     }
-    public void Gettrajectory(String empid, String starttime, String endtime){
+
+    public void Gettrajectory(String empid, String starttime, String endtime) {
         mBaiduMap.clear();
         String url = this.getString(R.string.requestUrl) + "GetEmptrajectory?empid=" + empid + "&&starttime=" + starttime + "&&endtime=" + endtime;
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -333,6 +344,7 @@ public class EmpTrajectoryMapActivity extends Activity {
                                     py = pointxy.split(",")[1];
                                     pointy = Double.parseDouble(py);
                                     LatLng llDot = new LatLng(pointy, pointx);
+
                                     options.add(llDot);
 
                                     //options.add(option);
@@ -347,7 +359,13 @@ public class EmpTrajectoryMapActivity extends Activity {
                                 OverlayOptions option = new PolylineOptions().width(10)
                                         .color(0xAAFF0000).points(options);
                                 mPolyline = (Polyline) mBaiduMap.addOverlay(option);
-                               // mBaiduMap.addOverlays(options);
+                                OverlayOptions markerOptions;
+                                markerOptions = new MarkerOptions().flat(true).anchor(0.5f, 0.5f)
+                                        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.arrow)).position(options.get(0))
+                                        .rotate((float) getAngle(0));
+                                mMoveMarker = (Marker) mBaiduMap.addOverlay(markerOptions);
+                                moveLooper();
+                                // mBaiduMap.addOverlays(options);
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -361,6 +379,143 @@ public class EmpTrajectoryMapActivity extends Activity {
         });
         mQueue.add(stringRequest);
     }
+
+    /**
+     * 根据点获取图标转的角度
+     */
+    private double getAngle(int startIndex) {
+        if ((startIndex + 1) >= mPolyline.getPoints().size()) {
+            throw new RuntimeException("index out of bonds");
+        }
+        LatLng startPoint = mPolyline.getPoints().get(startIndex);
+        LatLng endPoint = mPolyline.getPoints().get(startIndex + 1);
+        return getAngle(startPoint, endPoint);
+    }
+
+    /**
+     * 根据两点算取图标转的角度
+     */
+    private double getAngle(LatLng fromPoint, LatLng toPoint) {
+        double slope = getSlope(fromPoint, toPoint);
+        if (slope == Double.MAX_VALUE) {
+            if (toPoint.latitude > fromPoint.latitude) {
+                return 0;
+            } else {
+                return 180;
+            }
+        }
+        float deltAngle = 0;
+        if ((toPoint.latitude - fromPoint.latitude) * slope < 0) {
+            deltAngle = 180;
+        }
+        double radio = Math.atan(slope);
+        double angle = 180 * (radio / Math.PI) + deltAngle - 90;
+        return angle;
+    }
+
+    /**
+     * 根据点和斜率算取截距
+     */
+    private double getInterception(double slope, LatLng point) {
+
+        double interception = point.latitude - slope * point.longitude;
+        return interception;
+    }
+
+    /**
+     * 算斜率
+     */
+    private double getSlope(LatLng fromPoint, LatLng toPoint) {
+        if (toPoint.longitude == fromPoint.longitude) {
+            return Double.MAX_VALUE;
+        }
+        double slope = ((toPoint.latitude - fromPoint.latitude) / (toPoint.longitude - fromPoint.longitude));
+        return slope;
+
+    }
+
+    /**
+     * 循环进行移动逻辑
+     */
+    public void moveLooper() {
+        new Thread() {
+
+            public void run() {
+
+                while (true) {
+
+                    for (int i = 0; i < options.size() - 1; i++) {
+
+
+                        final LatLng startPoint = options.get(i);
+                        final LatLng endPoint = options.get(i + 1);
+                        mMoveMarker
+                                .setPosition(startPoint);
+
+                        mHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                // refresh marker's rotate
+                                if (mMapView == null) {
+                                    return;
+                                }
+                                mMoveMarker.setRotate((float) getAngle(startPoint,
+                                        endPoint));
+                            }
+                        });
+                        double slope = getSlope(startPoint, endPoint);
+                        // 是不是正向的标示
+                        boolean isReverse = (startPoint.latitude > endPoint.latitude);
+
+                        double intercept = getInterception(slope, startPoint);
+
+                        double xMoveDistance = isReverse ? getXMoveDistance(slope) :
+                                -1 * getXMoveDistance(slope);
+
+
+                        for (double j = startPoint.latitude; !((j > endPoint.latitude) ^ isReverse);
+                             j = j - xMoveDistance) {
+                            LatLng latLng = null;
+                            if (slope == Double.MAX_VALUE) {
+                                latLng = new LatLng(j, startPoint.longitude);
+                            } else {
+                                latLng = new LatLng(j, (j - intercept) / slope);
+                            }
+
+                            final LatLng finalLatLng = latLng;
+                            mHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (mMapView == null) {
+                                        return;
+                                    }
+                                    mMoveMarker.setPosition(finalLatLng);
+                                }
+                            });
+                            try {
+                                Thread.sleep(TIME_INTERVAL);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }.start();
+    }
+
+    /**
+     * 计算x方向每次移动的距离
+     */
+    private double getXMoveDistance(double slope) {
+        if (slope == Double.MAX_VALUE) {
+            return DISTANCE;
+        }
+        return Math.abs((DISTANCE * slope) / Math.sqrt(1 + slope * slope));
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -382,4 +537,7 @@ public class EmpTrajectoryMapActivity extends Activity {
         mMapView.onPause();
     }
 
+    private static final LatLng[] latlngs = new LatLng[]{
+
+    };
 }
